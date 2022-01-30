10c6844d6564b572432d3465455b597967be4751
Nick Rountree <nick@csvue.com>
2022-01-30 22:32:18 +1300

    Add Pancake library

---
+++ src/routes/charts/index.svelte
@@ -0,0 +1,20 @@
+<script context="module" lang="ts">
+	import Chart from '$lib/Chart.svelte';
+	import Chart2 from '$lib/Chart2.svx';
+</script>
+
+<svelte:head>
+	<title>Charts</title>
+</svelte:head>
+
+<Chart2 />
+
+<div class="chart">
+	<Chart />
+</div>
+
+<style>
+	.chart {
+		height: 250px;
+	}
+</style>
